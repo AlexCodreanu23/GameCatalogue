201828d1cef50527c6c465f5cc9ea7da9a211797
Alex Codreanu <126508471+AlexCodreanu23@users.noreply.github.com>
2024-09-01 15:16:23 +0300

    Create games.js

---
+++ frontend/src/data/games.js
@@ -0,0 +1,29 @@
+export const games = [
+    { 
+        id: 1, 
+        title: 'Fifa14', 
+        platform: 'Ps3', 
+        genre: 'Sports', 
+        price: '$59.99', 
+        releaseDate: 'September 15, 2013', 
+        image: '/images/fifa14.jpg' 
+    },
+    { 
+        id: 2, 
+        title: 'God of War', 
+        platform: 'PlayStation 4', 
+        genre: 'Action', 
+        price: '$39.99', 
+        releaseDate: 'April 20, 2018', 
+        image: '/images/gow.jpg' 
+    },
+    { 
+        id: 3, 
+        title: 'Cyberpunk 2077', 
+        platform: 'PC', 
+        genre: 'RPG', 
+        price: '$49.99', 
+        releaseDate: 'December 10, 2020', 
+        image: '/images/cyberpunk.jpg' 
+    }
+];
